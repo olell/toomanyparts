9631bf6acfba5f452ad1cc7309591649cf859935
olel <olel@olel.de>
2023-05-21 21:40:49 +0200

    Changed to automatically loading source

--- tomapa-server/tomapa/api/__init__.py
+++ tomapa-server/tomapa/api/__init__.py
@@ -1,11 +1,14 @@
 from marshmallow import ValidationError
 from flask_restful import abort
 
+from flask import request
 
-def load_schema_or_abort(schema, data):
+
+def load_schema_or_abort(schema, source="json"):
     """Loads the given marshmallow schema or abort the request"""
     try:
-        result = schema().load(data)
+        source = request.args if source == "args" else request.json
+        result = schema().load(source)
         if result is None:
             abort(404, message="Not found!")
         return result
