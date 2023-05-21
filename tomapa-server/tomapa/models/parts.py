57142fa9c63b439174001905a75ce98d7bd23fd5
olel <olel@olel.de>
2023-05-21 16:24:22 +0200

    Moved unit conversion to util file

--- tomapa-server/tomapa/models/parts.py
+++ tomapa-server/tomapa/models/parts.py
@@ -17,69 +17,6 @@ class Unit(Model):
     bigger = peewee.ForeignKeyField("self", null=True)
     bigger_div = peewee.IntegerField(default=1000)
 
-    def get_bigger(self, value):
-        """Returns the given value transformed to the next bigger unit"""
-        if self.bigger is None:
-            return value, self
-        else:
-            return value / self.bigger_div, self.bigger
-
-    def get_smaller(self, value):
-        """Returns the given value transformed to the next smaller unit"""
-        if self.smaller is None:
-            return value, self
-        else:
-            return value * self.smaller_mul, self.smaller
-
-    def _hr_bigger(self, value):
-        """Returns the given value transformed to the biggest unit where
-        the value is above 1"""
-        unit = self
-        while value >= 1:
-            _prev = unit
-            value, unit = unit.get_bigger(value)
-            if _prev == unit:
-                return value, unit
-        return unit.get_smaller(value)
-
-    def _hr_smaller(self, value):
-        """Returns the given value transformed to the smallest unit where
-        the value is below 1000"""
-        unit = self
-        while value < 1000:
-            _prev = unit
-            value, unit = unit.get_smaller(value)
-            if _prev == unit:
-                return value, unit
-        return unit.get_bigger(value)
-
-    def get_human_readable(self, value):
-        """Returns the given value converted to the "human readable" unit,
-        for example 1000nF will become 1uF or 0.1uF will become 100nF"""
-        if value >= 1 and value < 1000:
-            return value, self
-        elif value < 1:
-            return self._hr_smaller(value)
-        elif value >= 1000:
-            return self._hr_bigger(value)
-
-    def get_base(self, value):
-        if self.base is None or self.base == self:
-            return value, self
-        # This is a bit hacky, but in the first step I go to the biggest value and after that
-        # I go from the biggest to the smallest. If I reach the base at any point the value is
-        # returned
-        unit = self
-        while unit.smaller is not None:
-            value, unit = unit.get_smaller(value)
-            if self.base == unit:
-                return value, unit
-        while unit.bigger is not None:
-            value, unit = unit.get_bigger(value)
-            if self.base == unit:
-                return value, unit
-        return value, unit
-
     def __str__(self):
         return self.name
 
@@ -89,6 +26,7 @@ class Unit(Model):
 
 class PartCategory(Model):
     name = peewee.CharField()
+    parent = peewee.ForeignKeyField("self", null=True)
 
 
 class Part(Model):
---
+++ tomapa-server/tomapa/util/units.py
@@ -0,0 +1,64 @@
+def get_bigger(value, unit):
+    """Returns the given value transformed to the next bigger unit"""
+    if unit.bigger is None:
+        return value, unit
+    else:
+        return value / unit.bigger_div, unit.bigger
+
+
+def get_smaller(value, unit):
+    """Returns the given value transformed to the next smaller unit"""
+    if unit.smaller is None:
+        return value, unit
+    else:
+        return value * unit.smaller_mul, unit.smaller
+
+
+def _hr_bigger(value, unit):
+    """Returns the given value transformed to the biggest unit where
+    the value is above 1"""
+    while value >= 1:
+        _prev = unit
+        value, unit = get_bigger(value, unit)
+        if _prev == unit:
+            return value, unit
+    return get_smaller(value, unit)
+
+
+def _hr_smaller(value, unit):
+    """Returns the given value transformed to the smallest unit where
+    the value is below 1000"""
+    while value < 1000:
+        _prev = unit
+        value, unit = get_smaller(value, unit)
+        if _prev == unit:
+            return value, unit
+    return get_bigger(value, unit)
+
+
+def get_human_readable(value, unit):
+    """Returns the given value converted to the "human readable" unit,
+    for example 1000nF will become 1uF or 0.1uF will become 100nF"""
+    if value >= 1 and value < 1000:
+        return value, unit
+    elif value < 1:
+        return _hr_smaller(value, unit)
+    elif value >= 1000:
+        return _hr_bigger(value, unit)
+
+
+def get_base(value, unit):
+    if unit.base is None or unit.base == unit:
+        return value, unit
+    # This is a bit hacky, but in the first step I go to the biggest value and after that
+    # I go from the biggest to the smallest. If I reach the base at any point the value is
+    # returned
+    while unit.smaller is not None:
+        value, unit = get_smaller(value, unit)
+        if unit.base == unit:
+            return value, unit
+    while unit.bigger is not None:
+        value, unit = get_bigger(value, unit)
+        if unit.base == unit:
+            return value, unit
+    return value, unit
