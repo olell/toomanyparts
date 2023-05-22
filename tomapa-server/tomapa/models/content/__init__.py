082f5da5ea8a46df82d8cd76146818966043136b
olel <olel@olel.de>
2023-05-22 10:41:42 +0200

    Creating dummy storage location

--- tomapa-server/tomapa/models/content/__init__.py
+++ tomapa-server/tomapa/models/content/__init__.py
@@ -6,8 +6,14 @@ from tomapa.models.content.units import create_units
 from tomapa.models.content.properties import create_property_templates
 from tomapa.models.content.category import create_categories
 
+from tomapa.models.storage import StorageLocation
+
 
 def create_content():
     create_units()
     create_property_templates()
     create_categories()
+
+    # Create a single dummy storage location
+    dummy_location = StorageLocation(name="Default Storage Location")
+    dummy_location.save(1)
